bff07065c3ec617323796f2c8b269d6d22a06f5f
Fernando Henrique <103213962+FernandoDevH@users.noreply.github.com>
2024-09-27 12:59:44 -0300

    Atualização
    - Verificação de usuário existente para que não tenha duplicidades;
    - Criação de tempo para as mensagens de sucesso e erro;
    - Limpeza dos campos após o envio dos dados

--- app.py
+++ app.py
@@ -1,61 +1,66 @@
 import streamlit as st
 from datetime import datetime
-import time
 import json
 import os
-
+import time
+from streamlit import rerun
 st.title("Cadastro de Clientes")
 
 # Campos de entrada
-nome = st.text_input("Digite seu nome:")  # Campo para o nome do cliente
-endereco = st.text_input("Endereço:")  # Campo para o endereço do cliente
+if 'nome' not in st.session_state:
+    st.session_state.nome = ""
+if 'endereco' not in st.session_state:
+    st.session_state.endereco = ""
+
+st.session_state.nome = st.text_input("Digite seu nome:", value=st.session_state.nome)
+st.session_state.endereco = st.text_input("Endereço:", value=st.session_state.endereco)
 data_formatada = "DD/MM/YYYY"
 ano_nascimento = st.date_input("Data de Nascimento:", value=datetime.today(), format=data_formatada).strftime("%d/%m/%Y")  # Campo para a data de nascimento
-tipo_cliente = st.selectbox("Tipo do Cliente", ["Pessoa Física", "Pessoa Jurídica"])  # Seleção do tipo de cliente
+tipo_cliente = st.selectbox("Tipo do Cliente", ["Pessoa Física", "Pessoa Jurídica"])
 
-botao_cadastro = st.button("Enviar")  # Botão para enviar os dados
+botao_cadastro = st.button("Enviar")
 
-if botao_cadastro:  # Verifica se o botão foi clicado
-    # Criar um objeto com os dados do cliente
+if botao_cadastro:
     cliente = {
-        "Nome": nome,
-        "Endereço": endereco,
-        "Data de Nascimento": str(ano_nascimento),
+        "Nome": st.session_state.nome,
+        "Endereço": st.session_state.endereco,
+        "Data de Nascimento": ano_nascimento,
         "Tipo do Cliente": tipo_cliente
     }
     
-    # Verifica se o arquivo JSON já existe
-    if not os.path.isfile("clientes.json"):  # Se o arquivo "clientes.json" não existir
-        # Se não existir, cria uma lista vazia
+    if not os.path.isfile("clientes.json"):
         with open("clientes.json", "w", encoding="utf-8-sig") as arquivo:
-            json.dump([], arquivo)  # Cria um arquivo JSON vazio
-
-    # Lê os dados existentes do arquivo JSON
+            json.dump([], arquivo)
+        with open("cliente.csv", "w", encoding="utf-8-sig") as arquivocsv:
+            arquivocsv.write("Nome;Endereço;Data de Nascimento;Tipo do Cliente\n")
+    
     with open("clientes.json", "r+", encoding="utf-8-sig") as arquivo:
-        dados_existentes = json.load(arquivo)  # Carrega os dados existentes do arquivo JSON
-        dados_existentes.append(cliente)  # Adiciona o novo cliente à lista de dados existentes
-        arquivo.seek(0)  # Move o cursor para o início do arquivo para sobrescrever
-        json.dump(dados_existentes, arquivo, ensure_ascii=False, indent=4)  # Salva os dados com indentação para melhor legibilidade
-
-    # Salvar os dados em um arquivo CSV
-    # Salvar os dados em um arquivo CSV
-# Verifica se o arquivo já existe para não repetir os cabeçalhos
-    if not os.path.isfile("cliente.csv"):  # Se o arquivo não existir
-     with open("cliente.csv", "w", encoding="utf-8-sig") as arquivo:
-        # Escreve os cabeçalhos das colunas
-        arquivo.write("Nome;Endereço;Data de Nascimento;Tipo do Cliente\n")
-
-# Adiciona os dados do cliente
-    with open("cliente.csv", "a", encoding="utf-8-sig") as arquivo:
-    # Escreve os dados do cliente em uma nova linha
-        arquivo.write(';'.join([nome, endereco, ano_nascimento, tipo_cliente]) + "\n")
-
-    # Mensagem de sucesso
-    mensagem_sucesso = st.empty()  # Cria um espaço vazio para a mensagem
-    mensagem_sucesso.success("Cliente Cadastrado com Sucesso!")  # Exibe a mensagem de sucesso
-    time.sleep(3)  # Espera de 3 segundos
-    mensagem_sucesso.empty()  # Limpa a mensagem de sucesso
-    nome = ""
-    endereco = ""
-    ano_nascimento = datetime.today()  # Reseta a data de nascimento para a data atual
-    tipo_cliente = "Pessoa Física"  # Reseta o tipo de cliente para o padrão
+        dados_existentes = json.load(arquivo)
+        cliente_existente = any(
+            c["Nome"] == cliente["Nome"] and 
+            c["Endereço"] == cliente["Endereço"] and 
+            c["Data de Nascimento"] == cliente["Data de Nascimento"] 
+            for c in dados_existentes
+        )
+        
+        if not cliente_existente:
+            dados_existentes.append(cliente)
+            arquivo.seek(0)
+            json.dump(dados_existentes, arquivo, ensure_ascii=False, indent=4)
+            
+            with open("cliente.csv", "a", encoding="utf-8-sig") as arquivocsv:
+                arquivocsv.write(';'.join([st.session_state.nome, st.session_state.endereco, ano_nascimento, tipo_cliente]) + "\n")
+            mensagem = st.empty()
+            mensagem.success("Cliente Cadastrado com Sucesso!")
+            time.sleep(2)
+            mensagem.empty()
+            
+        else:
+            mensagem = st.empty()
+            mensagem.error("Cadastro já existe")
+            time.sleep(2)
+            mensagem.empty()
+    time.sleep(2)
+    st.session_state.nome = ""  # Limpa o nome
+    st.session_state.endereco = ""  # Limpa o nome
+    st.rerun()
